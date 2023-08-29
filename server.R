a7a880b7b4e05493d9d3e8b126af123aba11b223
kennedymwavu <mwavukennedy@gmail.com>
2023-08-29 03:22:38 +0300

    ft: render todos on load

--- server.R
+++ server.R
@@ -34,7 +34,6 @@ update_task <- \(todo_name, base_url, task_id, updated_item) {
 }
 server <- \(input, output, session) {
   num_of_tasks <- reactiveVal(0)
-  last_task_id <- reactiveVal(0)
   todo_list <- reactiveVal(
     get_todo_list(
       todo_name = todo_name,
@@ -43,10 +42,102 @@ server <- \(input, output, session) {
   )
   observeEvent(todo_list(), {
     num_of_tasks(length(todo_list()))
-    last_task_id(
-      if (num_of_tasks() > 0) todo_list()[[num_of_tasks()]]$id else 0
-    )
   })
+  observeEvent(todo_list(),
+    {
+      lapply(todo_list(), \(item) {
+        new_task_id <- item$id
+        new_task <- item$item
+        container_id <- paste0("task_", new_task_id, "_container")
+        text_input_id <- paste0("task_", new_task_id)
+        save_edits_id <- paste0("save_task_", new_task_id)
+        delete_task_id <- paste0("delete_task_", new_task_id)
+        ui <- tags$div(
+          id = container_id,
+          class = "d-flex mb-3",
+          textInput(
+            inputId = text_input_id,
+            label = NULL,
+            value = new_task,
+          ) |>
+            tagAppendAttributes(class = "mb-0 me-2 flex-grow-1"),
+          actionButton(
+            inputId = save_edits_id,
+            label = NULL,
+            icon = icon(
+              name = NULL,
+              class = "bi bi-check-lg"
+            ),
+            class = "btn-success btn-sm me-1 fs-5 border-0 rounded-circle",
+          ) |>
+            shinyjs::hidden() |>
+            bslib::tooltip("Save edits"),
+          actionButton(
+            inputId = delete_task_id,
+            label = NULL,
+            icon = icon(
+              name = NULL,
+              class = "bi bi-trash"
+            ),
+            class = "btn-danger btn-sm fs-5 border-0 rounded-circle"
+          ) |>
+            bslib::tooltip("Delete task")
+        ) |>
+          shinyjs::hidden()
+        insertUI(
+          selector = "#task_container",
+          where = "afterBegin",
+          ui = ui,
+          immediate = TRUE,
+          session = session
+        )
+        shinyjs::show(id = container_id, anim = TRUE)
+        observeEvent(input[[delete_task_id]],
+          {
+            delete_task(
+              todo_name = todo_name,
+              base_url = base_url,
+              task_id = new_task_id
+            )
+            todo_list(
+              get_todo_list(
+                todo_name = todo_name,
+                base_url = base_url
+              )
+            )
+            removeUI(
+              selector = paste0("#", container_id),
+              immediate = TRUE,
+              session = session
+            )
+          },
+          once = TRUE
+        )
+        # hide/show save edits btn:
+        observeEvent(input[[text_input_id]], {
+          shinyjs::toggle(
+            id = save_edits_id,
+            condition = input[[text_input_id]] != new_task,
+            anim = TRUE,
+            animType = "fade"
+          )
+        })
+        # save edits:
+        observeEvent(input[[save_edits_id]], {
+          update_task(todo_name, base_url, new_task_id, input[[text_input_id]])
+          todo_list(
+            get_todo_list(
+              todo_name = todo_name,
+              base_url = base_url
+            )
+          )
+          shinyjs::hide(id = save_edits_id, anim = TRUE, animType = "fade")
+        })
+        shinyjs::reset(id = "new_task")
+      })
+    },
+    once = TRUE
+  )
   # hide/show the no tasks container:
   observeEvent(num_of_tasks(), {
     shinyjs::toggle(
@@ -66,11 +157,16 @@ server <- \(input, output, session) {
   }) |>
     bindEvent(num_of_tasks())
   observeEvent(input$add_task, {
-    req(input$new_task)
-    new_task_id <- last_task_id() + 1
-    num_of_tasks(num_of_tasks() + 1)
-    last_task_id(new_task_id)
     new_task <- input$new_task
+    req(new_task)
+    add_task(todo_name = todo_name, base_url = base_url, task = new_task)
+    todo_list(
+      get_todo_list(
+        todo_name = todo_name,
+        base_url = base_url
+      )
+    )
+    new_task_id <- todo_list()[[length(todo_list())]]$id
     container_id <- paste0("task_", new_task_id, "_container")
     text_input_id <- paste0("task_", new_task_id)
     save_edits_id <- paste0("save_task_", new_task_id)
@@ -114,21 +210,26 @@ server <- \(input, output, session) {
       immediate = TRUE,
       session = session
     )
-    add_task(todo_name = todo_name, base_url = base_url, task = new_task)
     shinyjs::show(id = container_id, anim = TRUE)
     # once a task is deleted, reduce number of tasks:
-    observeEvent(input[[delete_task_id]], {
-        removeUI(
-          selector = paste0("#", container_id),
-          immediate = TRUE,
-          session = session
-        )
+    observeEvent(input[[delete_task_id]],
+      {
         delete_task(
           todo_name = todo_name,
           base_url = base_url,
           task_id = new_task_id
         )
-        num_of_tasks(num_of_tasks() - 1)
+        todo_list(
+          get_todo_list(
+            todo_name = todo_name,
+            base_url = base_url
+          )
+        )
+        removeUI(
+          selector = paste0("#", container_id),
+          immediate = TRUE,
+          session = session
+        )
       },
       once = TRUE
     )
@@ -144,6 +245,12 @@ server <- \(input, output, session) {
     # save edits:
     observeEvent(input[[save_edits_id]], {
       update_task(todo_name, base_url, new_task_id, input[[text_input_id]])
+      todo_list(
+        get_todo_list(
+          todo_name = todo_name,
+          base_url = base_url
+        )
+      )
       shinyjs::hide(id = save_edits_id, anim = TRUE, animType = "fade")
     })
     shinyjs::reset(id = "new_task")
